ff85978c54db6592421cc0e53e86ca1cc9cb255d
Esteban Lopez-Flores <esteban.lopezflores@gmail.com>
2016-12-27 22:22:46 -0600

    Worked on ch 1 - loops. left off before break and for loop section.

---
+++ Up-and-Going/Chapter-1/js/loops.js
@@ -0,0 +1,23 @@
+//while example
+var numOfCustomers = 10;
+
+while (numOfCustomers > 0) {
+  console.log( "How may I help you?" );
+
+  // help the customer ...
+
+  numOfCustomers = numOfCustomers - 1;
+}
+
+
+
+// do..while example
+numOfCustomers = 10
+
+do {
+  console.log( "How May I help you?" );
+
+  //help the customer
+
+  numOfCustomers = numOfCustomers - 1;
+} while (numOfCustomers > 0 );
--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
 ***************YOU DON'T KNOW JS***************
 
-  This is a record of my notes and code as I go through this series of books. Theses thoughts are not my own.
+  This is a record of my notes and code as I go through this series of books.
