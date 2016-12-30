ececa20202804ae68e606977501dddcaecd66410
Esteban Lopez-Flores <esteban.lopezflores@gmail.com>
2016-12-29 23:40:42 -0600

    Finished ch - loops section. Set up dev, css, and styling for titles, page, highlights,  and examples.

--- Up-and-Going/Chapter-1/js/loops.js
+++ Up-and-Going/Chapter-1/js/loops.js
@@ -21,3 +21,11 @@ do {
 
   numOfCustomers = numOfCustomers - 1;
 } while (numOfCustomers > 0 );
+
+
+// For Loop example
+
+for(var i = 0; i <=9; i = i + 1) {
+  console.log( i );
+}
+// 0 1 2 3 4 5 6 7 8 9
